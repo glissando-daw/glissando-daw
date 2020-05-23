eb2a89b8c79f3c95889a8eae80df04a920576258
Rodrigo García <yo@rodrigogarcia.me>
2020-05-23 21:48:22 +0200

    feat(glissando-app): set note in synth

--- packages/glissando-app/glissando-app.js
+++ packages/glissando-app/glissando-app.js
@@ -1,3 +1,19 @@
+const KEY_TO_NOTE = {
+  KeyA: 'C4',
+  KeyW: 'C#4',
+  KeyS: 'D4',
+  KeyE: 'D#4',
+  KeyD: 'E4',
+  KeyF: 'F4',
+  KeyT: 'F#4',
+  KeyG: 'G4',
+  KeyY: 'G#4',
+  KeyH: 'A4',
+  KeyU: 'A#4',
+  KeyJ: 'B4',
+  KeyK: 'C5',
+};
+
 const runWasm = async () => {
   const rust = await import('/web_modules/@glissando/glissando-synth/glissando_synth.js');
   await rust.default();
@@ -5,7 +21,14 @@ const runWasm = async () => {
   const synth = new rust.Osc();
   synth.suspend();
 
-  document.addEventListener('keydown', () => {
+  document.addEventListener('keydown', e => {
+    if (!(e.code in KEY_TO_NOTE)) {
+      return;
+    }
+
+    const note = KEY_TO_NOTE[e.code];
+    synth.set_note(note);
+
     synth.resume();
   });
 
