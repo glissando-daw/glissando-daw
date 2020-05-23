b27dad7d33beba397c9f6a7af255cd0d39b8cf43
Rodrigo García <yo@rodrigogarcia.me>
2020-05-23 21:18:57 +0200

    refactor(glissando-app): remove white noise and rename osc to synth

--- packages/glissando-app/glissando-app.js
+++ packages/glissando-app/glissando-app.js
@@ -2,23 +2,23 @@ const runWasm = async () => {
   const rust = await import('/web_modules/@glissando/glissando-synth/glissando_synth.js');
   await rust.default();
 
-  const osc = new rust.Osc();
-  osc.suspend();
+  const synth = new rust.Osc();
+  synth.suspend();
 
   document.getElementById('start').addEventListener('click', () => {
-    osc.resume();
+    synth.resume();
   });
 
   document.getElementById('stop').addEventListener('click', () => {
-    osc.suspend();
+    synth.suspend();
   });
 
   document.getElementById('volume-osc').addEventListener('input', e => {
-    osc.set_osc_amp(parseFloat(e.target.value));
+    synth.set_osc_amp(parseFloat(e.target.value));
   });
 
   document.getElementById('volume-src-buffer').addEventListener('input', e => {
-    osc.set_audio_buffer_amp(parseFloat(e.target.value));
+    synth.set_audio_buffer_amp(parseFloat(e.target.value));
   });
 };
 
