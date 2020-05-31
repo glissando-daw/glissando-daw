340b7070999fc40d1a1677361095bf18d344bea4
Sebastián Múnera <sfmunera@gmail.com>
2020-05-31 13:31:00 -0700

    UI: Implement slider and knob controls (#31)
    * Add slider
    
    * fix(glissando-app): normalize slider values
    
    * feat(glissando-app): implement knob control to adjust audio buffer amp
    
    * chore: add normalize.css again
    
    Co-authored-by: Rodrigo García <yo@rodrigogarcia.me>
    Co-authored-by: Sebastian Munera <smmunera@amazon.com>

---
+++ packages/glissando-knob/glissando-knob.js
@@ -0,0 +1,86 @@
+const SIZE = 64;
+const THUMB_WIDTH = SIZE / 5;
+const CENTER_X = SIZE / 2;
+const CENTER_Y = SIZE / 2;
+const OFFSET_ANGLE = (45 * Math.PI) / 180;
+const OFFSET_RADIUS = SIZE / 4 - THUMB_WIDTH / 2;
+const MIN_ANGLE = Math.PI - OFFSET_ANGLE;
+const MAX_ANGLE = 2 * Math.PI + OFFSET_ANGLE;
+
+class GlissandoKnob extends HTMLElement {
+  constructor() {
+    super();
+
+    this.thumbAngle = 1.5 * Math.PI;
+  }
+
+  connectedCallback() {
+    this.canvas = document.createElement('canvas');
+    this.canvas.width = SIZE;
+    this.canvas.height = SIZE;
+    this.canvas.addEventListener('click', e => this.onMouseClick(e));
+    this.appendChild(this.canvas);
+
+    this.ctx = this.canvas.getContext('2d');
+
+    this.draw();
+  }
+
+  draw() {
+    // background
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(0, 0, SIZE, SIZE);
+
+    this.ctx.strokeStyle = 'white';
+
+    // track
+    this.ctx.beginPath();
+    this.ctx.arc(CENTER_X, CENTER_Y, SIZE / 4, MIN_ANGLE, MAX_ANGLE);
+    this.ctx.stroke();
+
+    // thumb
+    this.ctx.beginPath();
+    const startX = CENTER_X + OFFSET_RADIUS * Math.cos(this.thumbAngle);
+    const startY = CENTER_Y + OFFSET_RADIUS * Math.sin(this.thumbAngle);
+    const endX = CENTER_X + (OFFSET_RADIUS + THUMB_WIDTH) * Math.cos(this.thumbAngle);
+    const endY = CENTER_Y + (OFFSET_RADIUS + THUMB_WIDTH) * Math.sin(this.thumbAngle);
+    this.ctx.moveTo(startX, startY);
+    this.ctx.lineTo(endX, endY);
+    this.ctx.stroke();
+  }
+
+  onMouseClick(e) {
+    const { x, y } = e;
+    const { top, left } = this.canvas.getBoundingClientRect();
+
+    const clickX = x - left - CENTER_X;
+    const clickY = y - top - CENTER_Y;
+    this.thumbAngle = Math.atan2(clickY, clickX);
+    // map atan2 range from [0, pi] to [0, pi], and from [-pi, 0] to [pi, 2*pi]
+    if (this.thumbAngle < 0) {
+      this.thumbAngle += 2.0 * Math.PI;
+    }
+    // map from [0, pi/4] to [2*pi, 5/4*pi] to make normalization continuous
+    if (this.thumbAngle >= 0 && this.thumbAngle <= OFFSET_ANGLE) {
+      this.thumbAngle += 2.0 * Math.PI;
+    }
+    // truncate not allowed values to min and max angle
+    if (OFFSET_ANGLE <= this.thumbAngle && this.thumbAngle <= 2 * OFFSET_ANGLE) {
+      this.thumbAngle = MAX_ANGLE;
+    }
+    if (2 * OFFSET_ANGLE <= this.thumbAngle && this.thumbAngle <= 3 * OFFSET_ANGLE) {
+      this.thumbAngle = MIN_ANGLE;
+    }
+    this.amount = (this.thumbAngle - MIN_ANGLE) / (MAX_ANGLE - MIN_ANGLE);
+
+    this.draw();
+
+    this.dispatchEvent(
+      new CustomEvent('change', {
+        detail: this.amount,
+      }),
+    );
+  }
+}
+
+customElements.define('glissando-knob', GlissandoKnob);
---
+++ packages/glissando-slider/glissando-slider.js
@@ -0,0 +1,62 @@
+const WIDTH = 64;
+const HEIGHT = 256;
+const TRACK_STROKE_SIZE = 1;
+const THUMB_STROKE_SIZE = 2;
+const THUMB_WIDTH = 24;
+
+class GlissandoSlider extends HTMLElement {
+  constructor() {
+    super();
+
+    this.thumbPosition = HEIGHT / 2;
+  }
+
+  connectedCallback() {
+    this.canvas = document.createElement('canvas');
+    this.canvas.width = WIDTH;
+    this.canvas.height = HEIGHT;
+    this.canvas.addEventListener('click', e => this.onMouseClick(e));
+    this.appendChild(this.canvas);
+
+    this.ctx = this.canvas.getContext('2d');
+
+    this.draw();
+  }
+
+  draw() {
+    // background
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    this.ctx.fillStyle = 'white';
+
+    // track
+    this.ctx.fillRect(WIDTH / 2, 0, TRACK_STROKE_SIZE, HEIGHT);
+
+    // thumb
+    this.ctx.fillRect(
+      WIDTH / 2 - THUMB_WIDTH / 2,
+      this.thumbPosition,
+      THUMB_WIDTH,
+      THUMB_STROKE_SIZE,
+    );
+  }
+
+  onMouseClick(e) {
+    const { y } = e;
+    const { top } = this.canvas.getBoundingClientRect();
+
+    this.thumbPosition = Math.round(y - top);
+    this.amount = 1.0 - this.thumbPosition / HEIGHT;
+
+    this.draw();
+
+    this.dispatchEvent(
+      new CustomEvent('change', {
+        detail: this.amount,
+      }),
+    );
+  }
+}
+
+customElements.define('glissando-slider', GlissandoSlider);
